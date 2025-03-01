80c91de4836443def8b3dc5e8de420015d7e533e
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2025-03-01 14:32:10 +0100

    API key security

---
+++ src/main/java/io/gocklkatz/schnoeschnoebe/security/AuthenticationFilter.java
@@ -0,0 +1,44 @@
+package io.gocklkatz.schnoeschnoebe.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AuthenticationFilter extends GenericFilterBean {
+
+    private final String auth_token;
+
+    public AuthenticationFilter(String auth_token) {
+        this.auth_token = auth_token;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        try {
+            Authentication authentication = new AuthenticationService(auth_token)
+                    .getAuthentication((HttpServletRequest) request);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception exp) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            PrintWriter writer = httpResponse.getWriter();
+            writer.print(exp.getMessage());
+            writer.flush();
+            writer.close();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/io/gocklkatz/schnoeschnoebe/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package io.gocklkatz.schnoeschnoebe.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${SCHNOE_API_KEY}")
+    private String SCHNOE_API_KEY;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.requestMatchers("/**").authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new AuthenticationFilter(SCHNOE_API_KEY), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+}
