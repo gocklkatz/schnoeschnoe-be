8c650e7cebec309acd14c3bd55dcd7c6a012aaa0
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2025-03-01 12:14:35 +0100

    Added log message

--- src/main/java/io/gocklkatz/schnoeschnoebe/controller/PromptController.java
+++ src/main/java/io/gocklkatz/schnoeschnoebe/controller/PromptController.java
@@ -1,5 +1,7 @@
 package io.gocklkatz.schnoeschnoebe.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,12 @@ import java.time.LocalDateTime;
 @RestController
 public class PromptController {
 
+    Logger logger = LoggerFactory.getLogger(PromptController.class);
+
     @GetMapping(path = "/prompt")
     public String prompt(String question) {
         LocalDateTime now = LocalDateTime.now();
+        logger.info("It is {}", now);
         return "You were here. " + now;
     }
 }
